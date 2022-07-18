8fe248faad4bb977451f480b65e128d95d50a4e1
Delroy Brown <delroybrown.db@gmail.com>
2022-07-18 17:12:12 +0100

    added product details funtionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from math import prod
+from django.shortcuts import get_object_or_404, render
 from .models import Product
 
 def all_products(request):
@@ -10,3 +11,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product' : product
+    }
+
+    return render(request, 'products/product_detail.html', context)
