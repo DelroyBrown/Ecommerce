093892d7bdda4e5629ff43543e0389002689fd00
Delroy Brown <delroybrown.db@gmail.com>
2022-07-18 17:23:27 +0100

    setting up search queries

--- products/views.py
+++ products/views.py
@@ -1,13 +1,26 @@
-from math import prod
-from django.shortcuts import get_object_or_404, render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render, redirect, reverse
+from django.db.models import Q
 from .models import Product
 
 def all_products(request):
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter a search query!")
+                return redirect(reverse('products'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
 
     context = {
-        'products' : products
+        'products' : products,
+        'search_form' : query,
     }
 
     return render(request, 'products/products.html', context)
