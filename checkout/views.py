6619592a223e9071200ad37b6289ced34282d77f
Delroy Brown <delroybrown.db@gmail.com>
2022-07-20 15:16:56 +0100

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_live_51HX5u6A1fnSIB3KmtTOvLA3c95Es4H5FQOBLVLaVWHbNcMrbRfkcZ9KWNyKJWaGEXaXL3JMRbmSPNs9cIQLmKpit00syKURUQl',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
